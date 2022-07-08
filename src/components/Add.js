5b7f48cc25697269a35a40fa66a14d9fdbdc44bb
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-09 02:51:34 +0600

    Create post modal added

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,17 +1,70 @@
-import { Tooltip, IconButton, Fab,  } from '@mui/material';
+import { Tooltip, IconButton, Fab, Modal, Box, styled, Typography, Avatar, TextField, Stack, ButtonGroup, Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import React from 'react';
+import React, {useState} from 'react';
+import { DateRange, EmojiEmotions, Image, PersonAdd, VideoCameraBack } from '@mui/icons-material';
+
+const MyModal = styled(Modal)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+});
+const UserBox = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    marginBottom: "20px"
+});
 
 const Add = () => {
+    const [open, setOpen] = useState(false);
     return (
         <>
-            <Tooltip title="Add" sx={{ position: "fixed", bottom: 20, left:{xs:"cal(48%)", md:30}}}>
+            <Tooltip onClick={e => setOpen(true)}
+            title="Add" sx={{ position: "fixed", bottom: 20, left:{xs:"calc(50% - 25px)", md:25}}}>
             <IconButton>
             <Fab color="primary" aria-label="add">
                 <AddIcon />
             </Fab>
             </IconButton>
             </Tooltip>
+            <MyModal
+                open={open}
+                onClose={e => setOpen(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <Box width={500} height={350} bgcolor='white' borderRadius={5} p={5}>
+                    <Typography variant="h6" color="gray" textAlign="center">Create Post</Typography>
+                    <UserBox>
+                        <Avatar alt="Remy Sharp" sx={{width: 30, height: 30}} src="https://i.ibb.co/7YJ2Jch/profile.png" />
+                        <Typography variant="span" fontWeight={500}>Shubrato </Typography>
+                    </UserBox>
+                    <TextField 
+                    sx={{width:"100%"}}
+                    id="standard-multiline-static"
+                    multiline
+                    rows={3}
+                    placeholder="What's on your mind ?"
+                    variant="standard"
+                    />
+                    <Stack direction="row" gap={1} mt={2} mb={3}>
+                        <EmojiEmotions color="primary" />
+                        <Image color="secondary" />
+                        <VideoCameraBack color="success" />
+                        <PersonAdd color="error" />
+                    </Stack>
+                    <ButtonGroup
+                        fullWidth
+                        variant="contained"
+                        aria-label="outlined primary button group"
+                    >
+                        <Button>Post</Button>
+                        <Button sx={{ width: "100px" }}>
+                        <DateRange />
+                        </Button>
+                    </ButtonGroup>
+                </Box>
+            </MyModal>
         </>
     );
 };
